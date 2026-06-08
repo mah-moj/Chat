7c27fafc9c8623dd9561453880a8fdfb550664be
MAHMOJ <mahdimojahed913@gmail.com>
2026-06-08 08:48:28 +0330

    Update database.php

--- Chat_app/Config/database.php
+++ Chat_app/Config/database.php
@@ -1,1 +1,15 @@
 
+<?php
+$host = 'localhost';
+$dbname = 'chat_app_advanced';
+$db_user = 'root';
+$db_pass = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $db_user, $db_pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    session_start();
+} catch(PDOException $e) {
+    die("خطا در اتصال: " . $e->getMessage());
+}
+?>
