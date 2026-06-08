78de630da92c3626eb4d79ee5ca4ab2a1b8fe219
MAHMOJ <mahdimojahed913@gmail.com>
2026-06-08 09:09:46 +0330

    Update index.php

--- Chat_app/index.php
+++ Chat_app/index.php
@@ -1,1 +1,13 @@
+<?php
+session_start();
 
+// اگر قبلاً وارد شده، به چت برود
+if (isset($_SESSION['user_id'])) {
+    header('Location: chat.php');
+    exit();
+}
+
+// در غیر این صورت به صفحه ورود برود
+header('Location: login.php');
+exit();
+?>
