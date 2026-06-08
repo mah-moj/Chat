31a282b3c1d99b655457ad884b0603edc390b259
MAHMOJ <mahdimojahed913@gmail.com>
2026-06-08 08:50:22 +0330

    Update auth.php

--- Chat_app/includes/auth.php
+++ Chat_app/includes/auth.php
@@ -1,1 +1,15 @@
 
+<?php
+require_once __DIR__ . '/../config/database.php';
+require_once __DIR__ . '/functions.php';
+
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+if (!isLoggedIn() && basename($_SERVER['PHP_SELF']) != 'login.php' && 
+    basename($_SERVER['PHP_SELF']) != 'register.php') {
+    header('Location: login.php');
+    exit();
+}
+?>
